732ef1c9899c56c0e8e723def3ee69dee4f9abd0
Ken Goad <goadke@g.cofc.edu>
2019-11-06 12:37:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
 def unsafe?(speed)
-
+  
+    if (speed > 60) then true
+    
+    elsif speed < 40 then true
+    
+    else return false if speed = 40..60
+      
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	 
+	 return true ? 
+	 return false ? speed = 40..60
+	 
 end
 	
 
